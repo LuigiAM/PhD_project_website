6a8e337f7616941d667e9d6bf44efc0802a01670
Luigi Andrea M. <luigiandreamoretti@gmail.com>
2025-06-10 14:48:27 +0100

    Fix astro config site and base paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://LuigiAM.github.io', // Your username
+  base: '/PhD_project_website',      // The name of your repository
+});
